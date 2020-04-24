458791009665b9df411ca606149b59a84ce39a92
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-04-24 18:24:33 +0930

    Added extra examples in des.info and updated to write files and plot out

--- R/des.info.R
+++ R/des.info.R
@@ -1,40 +1,96 @@
 #' Produces graph of design layout, skeletal ANOVA table and data frame with complete design
 #'
-#' @param design.obj An \code{agricolae} design object.
+#' @param design.obj An `agricolae` design object.
 #' @param nrows The number of rows in the design.
 #' @param ncols The number of columns in the design.
 #' @param brows For RCBD only. The number of rows in a block.
 #' @param bcols For RCBD only. The number of columns in a block.
-#' @param return.seed Logical (default TRUE). Output the seed used in the design?
-#' @param rotation Rotate the text output as Treatments within the plot. Allows for easier reading of long treatment labels.
+#' @param rotation Rotate the text output as Treatments within the plot. Allows for easier reading of long treatment labels. Takes positive and negative values being number of degrees of rotation from horizontal.
 #' @param size Increase or decrease the text size within the plot for treatment labels. Numeric with default value of 4.
-#' @param margin Logical (default FALSE). Expand the plot to the edges of the plotting area i.e. remove whitespace between plot and axes.
+#' @param margin Logical (default FALSE). Expand the plot to the edges of the plotting area i.e. remove white space between plot and axes.
+#' @param save One of `FALSE` (default)/`"none"`, `TRUE`/`"both"`, `"plot"` or `"workbook"`. Specifies which output to save.
+#' @param savename A filename for the design to be saved to. Default is the type of the design combined with "_design".
+#' @param return.seed Logical (default TRUE). Output the seed used in the design?
 #' @param quiet Logical (default FALSE). Return the objects without printing output.
+#' @param ... Additional parameters passed to [ggplot2::ggsave()] for saving the plot.
+#'
+#' @details If `save = TRUE` (or `"both"`), both the plot and the workbook will be saved to the current working directory. If one of either `"plot"` or `"workbook"` is specified, only that output is saved. If `save = FALSE` (the default, or equivalently `"none"`), nothing will be output.
+#' @details `...` allows extra arguments to be passed to ggsave for output of the plot. The details of possible arguments can be found in  [ggplot2::ggsave()].
 #'
 #' @return A list containing a data frame with the complete design and a ggplot object with plot layout.
 #'
 #' @examples
 #' library(agricolae)
+#'
+#' # Completely Randomised Design
 #' trt <- c(1,5,10,20)
 #' rep <- 5
 #' outdesign <- design.crd(trt = trt, r=rep, seed = 42)
-#' des.out <- des.info(design.obj = outdesign, nrows = 4,
-#'                     ncols = 5, brows = NA, bcols = NA)
+#' des.out <- des.info(design.obj = outdesign, nrows = 4, ncols = 5)
 #'
+#' # Randomised Complete Block Design
 #' trt <- LETTERS[1:11]
 #' rep <- 4
 #' outdesign <- design.rcbd(trt = trt, r = rep, seed = 42)
 #' des.out <- des.info(design.obj = outdesign, nrows = 11,
 #'                     ncols = 4, brows = 11, bcols = 1)
 #'
+#' # Latin Square Design
+#' trt <- c("S1", "S2", "S3", "S4")
+#' outdesign <- design.lsd(trt)
+#' des.out <- des.info(design.obj = outdesign, nrows = 4, ncols = 4)
+#'
+#' # Factorial Design (Crossed, Completely Randomised)
+#' trt <- c(3, 2) # Factorial 3 x 2
+#' rep <- 3
+#' outdesign <- design.ab(trt, r = rep, design = "crd")
+#' des.out <- des.info(design.obj = outdesign, nrows = 6, ncols = 3)
+#'
+#' # Factorial Design (Nested, Latin Square)
+#' trt <- c("A1", "A2", "A3", "A4", "B1", "B2", "B3")
+#' outdesign <- design.lsd(trt)
+#' des.out <- des.info(design.obj = outdesign, nrows = 7, ncols = 7)
+#'
+#' # Split plot design
+#' trt1 <- c("A", "B")
+#' trt2 <- 1:4
+#' rep <- 4
+#' outdesign <- design.split(trt1, trt2, r = rep)
+#' des.out <- des.info(design.obj = outdesign, nrows = 8, ncols = 4)
+#'
 #' @export
-des.info <- function(design.obj, nrows, ncols, brows = NA, bcols = NA, return.seed = TRUE, rotation = 0, size = 4, margin = FALSE, quiet = FALSE){
+#' @importFrom graphics plot
+#' @importFrom ggplot2 ggsave
+#' @importFrom utils write.csv
+des.info <- function(design.obj, nrows, ncols, brows = NA, bcols = NA, rotation = 0, size = 4, margin = FALSE, save = FALSE, savename = paste0(design.obj$parameters$design, "_design"), return.seed = TRUE, quiet = FALSE, ...){
+
+    #Error checking of inputs
+    if(design.obj$parameters$design == "rcbd" & anyNA(c(brows, bcols))) {
+        stop("Design has blocks so brows and bcols must be supplied.")
+    }
+    else if(design.obj$parameters$design == "factorial") {
+        if(design.obj$parameters$applied == "rcbd" &
+           anyNA(c(brows, bcols))) {
+            stop("Design has blocks so brows and bcols must be supplied.")
+        }
+    }
+
     info <- plot.des(design.obj, nrows, ncols, brows, bcols, rotation, size, margin, quiet, return.seed = return.seed)
     info$satab <- satab(design.obj)
 
     if(!quiet) {
         satab(design.obj)
+        plot(info$plot.des)
     }
+
+    if(!is.logical(save)) {
+        output <- match.arg(tolower(save), c("none", "both", "plot", "workbook"))
+    }
+    else if(save) {
+        ggplot2::ggsave(filename = savename, device = "pdf", ...)
+        write.csv(info$design, file = "")
+    }
+
 
     return(info)
 }
