44ec4c5105e6ab01ab7cf981d22aa8a960b513a9
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-04-26 13:54:17 +0930

    Started custom expectation file

--- tests/testthat/setup-des.R
+++ tests/testthat/setup-des.R
@@ -4,3 +4,7 @@ trt <- LETTERS[1:11]
 rep <- 4
 outdesign <- design.rcbd(trt = trt, r = rep, seed = 42)
 outdesign_crd <- design.crd(trt = trt, r = rep, seed = 42)
+
+expect_file <- function(expr, file) {
+
+}
