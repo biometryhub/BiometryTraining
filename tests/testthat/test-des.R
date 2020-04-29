17daf51ed4b26d2a24913cec96f11cf70be30e4f
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-04-29 19:03:38 +0930

    Updated config

--- tests/testthat/test-des.R
+++ tests/testthat/test-des.R
@@ -161,7 +161,7 @@ test_that("quiet = F prints output and plot", {
                   "Source of Variation                     df")
     x <- des.info(design.obj = outdesign_crd, nrows = 11, ncols = 4, quiet = T)
 
-    if(R.version.string >= 3.3) {
+    if(R.version.string >= 3.5) {
         vdiffr::expect_doppelganger("des_info output", x$plot.des)
     }
 })
