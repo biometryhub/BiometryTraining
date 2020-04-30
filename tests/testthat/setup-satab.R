8ab9caa6ada35579499b5c95feb46d5ba1c759c0
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-04-30 13:12:52 +0930

    Updated to add coverage for missed cases in satab

--- tests/testthat/setup-satab.R
+++ tests/testthat/setup-satab.R
@@ -18,6 +18,14 @@ trt <- c(3, 2) # Factorial 3 x 2
 rep <- 3
 outdesign_crossed_satab <- design.ab(trt, r = rep, design = "crd")
 
+trt <- c(3, 2) # Factorial 3 x 2
+rep <- 3
+outdesign_crossed_rcbd_satab <- design.ab(trt, r = rep, design = "rcbd")
+
+trt <- c(3, 2) # Factorial 3 x 2
+rep <- 3
+outdesign_crossed_lsd_satab <- design.ab(trt, r = rep, design = "lsd")
+
 # Factorial Design (Nested, Latin Square)
 trt <- c("A1", "A2", "A3", "A4", "B1", "B2", "B3")
 outdesign_nested_satab <- design.lsd(trt)
