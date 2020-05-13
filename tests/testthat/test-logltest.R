437b3b591d0abd184a602d98dddcfdae6abbec0c
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-05-13 11:49:59 +0930

    Updated todo list

---
+++ tests/testthat/test-logltest.R
@@ -0,0 +1,5 @@
+load("../dat.asr.Rdata")
+
+test_that("function works", {
+  expect_equal(2 * 2, 4)
+})
---
+++ tests/testthat/test-tuk.R
@@ -0,0 +1,6 @@
+dat_asr_tuk <- load("../dat.asr.Rdata")
+
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
