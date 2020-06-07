3c57370e0ae5413ae5d70151bbd5e56f1a37771f
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-06-07 21:08:19 +0930

    Added check for existing install

--- R/install_asreml.R
+++ R/install_asreml.R
@@ -2,12 +2,15 @@
 #'
 #' @param quiet Should package be installed quietyly? Default is `TRUE`.
 #'
-#' @return
+#' @return `TRUE` if asreml installed successfully or already present, `FALSE` otherwise.
 #' @export
 #'
 #' @keywords internal
 #'
 install_asreml <- function(quiet = TRUE) {
+    if(require(asreml)) {
+        return(TRUE)
+    }
 
     opts <- c("win_3.5", "mac_3.5", "linux_3.5", "win_3.6", "mac_3.6", "linux_3.6",
               "win_4", "mac_4", "linux_4")
