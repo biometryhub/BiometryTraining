73f7ae698ca40460539c0f933c12445045043f7a
Sharon Nielsen <65264926+snstats@users.noreply.github.com>
2020-07-30 11:44:39 +1000

    fixed ranking order (I hope)

--- R/mct.R
+++ R/mct.R
@@ -1,218 +1,40 @@
-#' Multiple Comparison Tests
-#'
-#' A function for comparing and ranking predicted means with Tukey's Honest Significant Difference (HSD) Test.
-#'
-#' @param model.obj An ASReml-R or aov model object.
-#' @param pred.obj An ASReml-R prediction object with `sed = TRUE`.
-#' @param sig The significance level, numeric between 0 and 1. Default is 0.05.
-#' @param pred Name of predictor variable as string.
-#' @param int.type The type of confidence interval to calculate. One of `ci`, `1se` or `2se`. Default is `ci`.
-#' @param trans Transformation that was applied to the response variable. One of `log`, `sqrt`, `logit` or `inverse`. Default is `NA`.
-#' @param offset Numeric offset applied to response variable prior to transformation. Default is `NA`.
-#' @param round Controls rounding of decimal places in output. Default is 2 decimal places.
-#'
-#' @importFrom multcompView multcompLetters
-#' @importFrom agricolae LSD.test HSD.test
-#' @importFrom predictmeans predictmeans
-#' @importFrom stats predict
-#'
-#' @return A list containing a data frame `pred.tab` consisting of predicted means, standard errors, confidence interval upper and lower bounds, and significant group allocations.
-#'
-#' @examples
-#' \dontrun{
-#' library(asreml)
-#'
-#' #Fit ASreml Model
-#' model.asr <- asreml(yield ~ Nitrogen + Variety + Nitrogen:Variety,
-#'                     random = ~ Blocks + Blocks:Wplots,
-#'                     residual = ~ units,
-#'                     data = asreml::oats)
-#'
-#' wald(model.asr) #Nitrogen main effect significant
-#'
-#' #Calculate predicted means
-#' pred.asr <- predict(model.asr, classify = "Nitrogen", sed = TRUE)
-#'
-#' #Determine ranking and groups according to Tukey's Test
-#' tuk.rank <- mct.out(model.obj = model.asr, pred.obj = pred.asr, sig = 0.05,
-#'                     int.type = "ci", pred = "Nitrogen")
-#'
-#' tuk.rank}
-#'
-#' @export
-#'
-mct.out <- function(model.obj, pred.obj, sig = 0.05, pred, int.type = "ci", trans = NA, offset = NA, round = 2){
-
-  if(class(model.obj)[1] == "asreml"){
-
-    #For use with asreml 4+
-    if(packageVersion("asreml") > 4) {
-      pp <- pred.obj$pvals
-
-      # Check that the prediction object was created with the sed matrix
-      if(is.null(pred.obj$sed)) {
-        stop("Prediction object (pred.obj) must be created with argument sed = TRUE.")
-      }
-
-      sed <- pred.obj$sed
+library(agricolae)
+reorder<-function(inV){
+    collapsed <- paste(inV,sep="",collapse = "")
+    u <- unique(strsplit(collapsed,"")[[1]])
+    if(length(u)<2){
+        return(inV)
     }
-
-
-    pp <- pp[!is.na(pp$predicted.value),]
-    pp$status <- NULL
-
-    ifelse(grepl(":", pred),
-           pp$Names <- apply(pp[,unlist(strsplit(pred, ":"))], 1, paste, collapse = "_"),
-           pp$Names <- pp[[pred]])
-
-    zz <- as.numeric(row.names(pp[!is.na(pp$predicted.value),]))
-
-    SED <- sed[zz,zz]
-    Mean <- pp$predicted.value
-    Names <-  as.character(pp$Names)
-    nedf <- model.obj$nedf
-    crit.val <- 1/sqrt(2)* qtukey((1-sig), nrow(pp), nedf)*SED
-  } else {
-
-
-    pred.out <- predictmeans::predictmeans(model.obj, pred, mplot = FALSE)
-    pred.out$mean_table <- pred.out$mean_table[,!grepl("95", names(pred.out$mean_table))]
-    sed <- pred.out$`Standard Error of Differences`[1]
-    pp <- pred.out$mean_table
-    names(pp)[names(pp) == "Predicted means"] <- "predicted.value"
-    names(pp)[names(pp) == "Standard error"] <- "std.error"
-
-    SED <- matrix(data = sed, nrow = nrow(pp), ncol = nrow(pp))
-    diag(SED) <- NA
-    Mean <- pp$predicted.value
-    ifelse(grepl(":", pred),
-           pp$Names <- apply(pp[,unlist(strsplit(pred, ":"))], 1, paste, collapse = "_"),
-           pp$Names <- pp[[pred]])
-
-    Names <-  as.character(pp$Names)
-    nedf <- pp$Df[1]
-    crit.val <- 1/sqrt(2)* qtukey((1-sig), nrow(pp), nedf)*SED
-
-  }
-
-
-
-    # Determine pairs that are significantly different
-    diffs <- abs(outer(Mean, Mean,"-")) > crit.val
-    diffs <- diffs[lower.tri(diffs)]
-
-    # Create a vector of treatment comparison names
-    m <- outer(pp$Names, pp$Names, paste, sep="-")
-    m <- m[lower.tri(m)]
-
-
-    names(diffs) <- m
-
-
-    ll <- multcompView::multcompLetters(diffs, threshold = sig, compare = ">", reversed = TRUE)
-
-    rr <- data.frame(groups = ll$Letters)
-    rr$Names <- row.names(rr)
-
-
-    pp.tab <- merge(pp,rr)
-
-
-    if(!is.na(trans)){
-
-      if(trans == "log"){
-        pp.tab$PredictedValue <- exp(pp.tab$predicted.value) - ifelse(!is.na(offset), offset, 0)
-        pp.tab$ApproxSE <- abs(pp.tab$std.error)*pp.tab$PredictedValue
-        if(int.type == "ci"){
-        pp.tab$ci <- qt(p = sig, nedf, lower.tail = FALSE) * pp.tab$std.error
-        }
-        if(int.type == "1se"){
-          pp.tab$ci <- pp.tab$std.error
-        }
-        if(int.type == "2se"){
-          pp.tab$ci <- 2*pp.tab$std.error
-        }
-        pp.tab$low <- exp(pp.tab$predicted.value - pp.tab$ci) - ifelse(!is.na(offset), offset, 0)
-        pp.tab$up <- exp(pp.tab$predicted.value + pp.tab$ci) - ifelse(!is.na(offset), offset, 0)
-      }
-
-      if(trans == "sqrt"){
-        pp.tab$PredictedValue <- (pp.tab$predicted.value)^2 - ifelse(!is.na(offset), offset, 0)
-        pp.tab$ApproxSE <- 2*abs(pp.tab$std.error)*sqrt(pp.tab$PredictedValue)
-        if(int.type == "ci"){
-          pp.tab$ci <- qt(p = sig, nedf, lower.tail = FALSE) * pp.tab$std.error
-        }
-        if(int.type == "1se"){
-          pp.tab$ci <- pp.tab$std.error
-        }
-        if(int.type == "2se"){
-          pp.tab$ci <- 2*pp.tab$std.error
-        }
-        pp.tab$low <- (pp.tab$predicted.value - pp.tab$ci)^2 - ifelse(!is.na(offset), offset, 0)
-        pp.tab$up <- (pp.tab$predicted.value + pp.tab$ci)^2 - ifelse(!is.na(offset), offset, 0)
-      }
-
-
-      if(trans == "logit"){
-        pp.tab$PredictedValue <- exp(pp.tab$predicted.value)/(1 + exp(pp.tab$predicted.value))
-        pp.tab$ApproxSE <- pp.tab$PredictedValue * (1 - pp.tab$PredictedValue)* abs(pp.tab$std.error)
-        if(int.type == "ci"){
-          pp.tab$ci <- qt(p = sig, nedf, lower.tail = FALSE) * pp.tab$std.error
-        }
-        if(int.type == "1se"){
-          pp.tab$ci <- pp.tab$std.error
-        }
-        if(int.type == "2se"){
-          pp.tab$ci <- 2*pp.tab$std.error
-        }
-        pp.tab$ll <- pp.tab$predicted.value - pp.tab$ci
-        pp.tab$low <- exp(pp.tab$ll)/(1 + exp(pp.tab$ll))
-        pp.tab$uu <- pp.tab$predicted.value + pp.tab$ci
-        pp.tab$up <- exp(pp.tab$uu)/(1 + exp(pp.tab$uu))
-
-        pp.tab$ll <- NULL
-        pp.tab$uu <- NULL
-        pp.tab$transformed.value <- NULL
-        pp.tab$approx.se <- NULL
-
-      }
-
-      if(trans == "inverse"){
-        pp.tab$PredictedValue <- 1/pp.tab$predicted.value
-        pp.tab$ApproxSE <- abs(pp.tab$std.error)*pp.tab$PredictedValue^2
-        if(int.type == "ci"){
-          pp.tab$ci <- qt(p = sig, nedf, lower.tail = FALSE) * pp.tab$std.error
-        }
-        if(int.type == "1se"){
-          pp.tab$ci <- pp.tab$std.error
-        }
-        if(int.type == "2se"){
-          pp.tab$ci <- 2*pp.tab$std.error
-        }
-        pp.tab$low <- 1/(pp.tab$predicted.value - pp.tab$ci)
-        pp.tab$up <- 1/(pp.tab$predicted.value + pp.tab$ci)
-      }
-    } else {
-
-      if(int.type == "ci"){
-        pp.tab$ci <- qt(p = sig, nedf, lower.tail = FALSE) * pp.tab$std.error
-      }
-      if(int.type == "1se"){
-        pp.tab$ci <- pp.tab$std.error
-      }
-      if(int.type == "2se"){
-        pp.tab$ci <- 2*pp.tab$std.error
-      }
-      pp.tab$low <- pp.tab$predicted.value - pp.tab$ci
-      pp.tab$up <- pp.tab$predicted.value + pp.tab$ci
-
+    u <- u[order(u)]
+    m <- matrix(nrow=NROW(inV),ncol=length(u))
+    m[]<-F
+    for(i in 1:length(inV)){
+        s <- strsplit(inV[i],"")[[1]]
+        index <- match(s,u)
+        m[i,index] <- T
     }
-
-  pp.tab$Names <- NULL
-
-
-  # pp.tab <- dplyr::mutate(pp.tab, dplyr::across(where(is.numeric), base::round, digits = round))
-  pp.tab <- rapply(object = pp.tab, f = base::round, classes = "numeric", how = "replace", digits = round)
-
-  return(pp.tab)
+    for(i in 1:(length(u)-1)){
+        firstColT <- match(T,m[,i])[1] #first row with true in current column
+        firstT <- match(T,rowSums(m[,i:length(u)] > 0))[1] #first row with true in rest
+        if(firstT < firstColT){
+            colT <- match(T,m[firstT,i:length(u)])[1]
+            colT <- colT + i - 1 #correct index for leftout columns in match
+            tmp <- m[,colT]
+            m[,colT] <- m[,i]
+            m[,i] <- tmp
+        }
+    }
+    res <- vector(mode = "character", length=length("trt"))
+    for(i in 1:length(inV)){
+        l <- u[m[i,]]
+        res[i] <- paste(l,sep="",collapse = "")
+    }
+    return(res)
 }
+
+
+
+fit <- lm(Sepal.Length ~ Species, data = iris)
+a <- HSD.test(fit, "Species", group=T, console=F)$groups
+a <- a[rev(rownames(a)),] #order the result the way you want
+a$M <- reorder(as.character(a$M))
