485218f7e2e3c447bcf22b7872976d59a976fff2
Sharon Nielsen <65264926+snstats@users.noreply.github.com>
2020-08-03 10:27:05 +1000

    more fixing of letters

--- R/mct.R
+++ R/mct.R
@@ -109,7 +109,7 @@ mct.out <- function(model.obj, pred.obj, sig = 0.05, pred, int.type = "ci", tran
     names(diffs) <- m
 
 
-    ll <- multcompView::multcompLetters3(x = diffs, z = pred, y = predicted.value, data = pp)
+    ll <- multcompView::multcompLetters3(pred, "predicted.value", diffs, pp)
 
     rr <- data.frame(groups = ll$Letters)
     rr$Names <- row.names(rr)
