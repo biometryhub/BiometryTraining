9e0c64a25f2e95d0694a484b622a104a1da00628
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2020-08-26 10:31:04 +0930

    Updated readme (#15)
    * Merged master into dev and started adding utility functions
    
    * Updated to include install on R 4.1 on Windows
    
    * Trying to fix build errors
    
    * fixing letter order
    
    * more fixing of letters
    
    * fixing to work with interactions
    
    * Updated install_asreml function to handle upgrades and use shortlinks
    
    * Updated to use tidyverse style
    
    * Fixed build issues
    
    * Updated Description to remove the work "package"
    
    * Removed tidyselect
    
    * Stop checking on version 3.5
    
    * Updated readme
    
    * Updated readme and namespace
    
    * Updates to install function - needs more work
    
    * Updated install_asreml to check for existing file and keep if desired
    
    * Install data.table if not installed already
    
    * Trying to fix build errors
    
    * More tests for asreml
    
    * Add missing dependancies for data.table on Linux
    
    * Trying to install packages to enable data.table installation
    
    * Fixing actions config
    
    * Spelling error
    
    * Turns out these packages are already installed
    
    * Update mct.out to allow changing order direction
    
    * Suppress asreml output
    
    * Allow keeping download and forcing to install
    
    * changes to lucid call
    
    * Trying to fix tests
    
    * Updated install_asreml with test cases working
    
    * Install from source on mac and linux
    
    * Corrected for devel version
    
    * Try and install dependencies first
    
    * Add a mirror
    
    * round results
    
    * Updated readme to use drat
    
    Co-authored-by: Sharon Nielsen <65264926+snstats@users.noreply.github.com>

--- R/logltest.R
+++ R/logltest.R
@@ -134,7 +134,8 @@ logl.test <- function(model.obj, rand.terms, resid.terms) {
         # Logl test
         ll.test <- asremlPlus::REMLRT(h1.asreml.obj = model.obj, h0.asreml.obj = model.obj1)$p
 
-        result.df <- data.frame(Term = tt[i], LogLRT.pvalue = ll.test)
+        result.df <- data.frame(Term = tt[i], LogLRT.pvalue = round(ll.test,3))
+
         test.df <- rbind(test.df, result.df)
       }
     }
