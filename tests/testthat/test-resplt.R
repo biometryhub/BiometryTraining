7281c70ac4f6efebef19ef296501478c043538c5
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-03-25 20:06:37 +1030

    Updated resplot tests

--- tests/testthat/test-resplt.R
+++ tests/testthat/test-resplt.R
@@ -7,26 +7,24 @@
 
 
 test_that("plotting works", {
-  print(getwd())
   dat.aov <- aov(Petal.Length ~ Petal.Width, data = iris)
 
   expect_error(resplt(1:10), "mod.obj must be an aov or asreml object")
-  # expect_error(resplt(dat.aov), "mod.obj must be an aov or asreml object")
   p1 <- resplot(dat.aov)
-
-
-  load("../complexmodel.Rdata")
-  p1_multi <- resplt(final.m.asr)[[1]]
-  p2_multi <- resplot(final.m.asr)[[2]]
-  p3_multi <- resplot(final.m.asr)[[3]]
-  # ggsave("test-plot.png", p1)
-
-  # expect_plots_identical("test-plot.png", "../figs/resplt/resplt-aov.png")
 
   skip_if(getRversion() < 3.6)
   vdiffr::expect_doppelganger(title = "Resplot for aov", p1)
-  vdiffr::expect_doppelganger(title = "Resplot for asreml pt 1", p1_multi)
-  vdiffr::expect_doppelganger(title = "Resplot for asreml pt 2", p2_multi)
-  vdiffr::expect_doppelganger(title = "Resplot for asreml pt 3", p3_multi)
+
+  skip_if_not_installed("asreml")
+  load("../complexmodel.Rdata")
+  p1_multi <- resplt(final.m.asr)#[[1]]
+  # p2_multi <- resplot(final.m.asr)[[2]]
+  # p3_multi <- resplot(final.m.asr)[[3]]
+  # ggsave("test-plot.png", p1)
+
+  # expect_plots_identical("test-plot.png", "../figs/resplt/resplt-aov.png")
+  vdiffr::expect_doppelganger(title = "Resplot for asreml pt 1", p1_multi[[1]])
+  vdiffr::expect_doppelganger(title = "Resplot for asreml pt 2", p1_multi[[2]])
+  vdiffr::expect_doppelganger(title = "Resplot for asreml pt 3", p1_multi[[3]])
   # vdiffr::manage_cases()
 })
