91ca5c7a30297c9613c1af7724a7df52dee4ec91
Sharon Nielsen <65264926+snstats@users.noreply.github.com>
2021-07-26 15:37:10 +1000

    using cowplot to get graphs together

--- R/variogram.r
+++ R/variogram.r
@@ -18,37 +18,41 @@ vario <- function(mod.obj){
         stop("mod.obj must be an asreml object")
     }
 
-    aa <- varioGram(mod.obj)
+    aa <- asreml::varioGram(mod.obj)
     xnam <- names(aa)[2]
     ynam <- names(aa)[1]
-    fld <- interp(y = aa[,1], x = aa[,2], z = aa$gamma)
-    gdat <- interp2xyz(fld, data.frame = TRUE)
+    fld <- akima::interp(y = aa[,1], x = aa[,2], z = aa$gamma)
+    gdat <- akima::interp2xyz(fld, data.frame = TRUE)
 
-
-        a <- ggplot2::ggplot(gdat, aes(x = x, y = y, z = z, fill = z)) +
+        a <- ggplot2::ggplot(gdat, ggplot2::aes(x = y, y = x, z = z, fill = z)) +
         ggplot2::geom_tile(alpha = 0.6) +
         ggplot2::coord_equal() +
         ggplot2::geom_contour(color = "white", alpha = 0.5) +
-        ggplot2::scale_fill_gradientn(colours=rainbow(100)) +
-        ggplot2::theme_bw() +
-        ggplot2::scale_x_continuous(expand = c(0, 0), breaks = seq(1, max(gdat$x), 1)) +
-        ggplot2::scale_y_continuous(expand = c(0, 0), breaks = seq(1, max(gdat$y), 1)) +
+        ggplot2::scale_fill_gradientn(colours = rainbow(100)) +
+        ggplot2::theme_bw(base_size = 6) +
+        ggplot2::scale_y_continuous(expand = c(0, 0), breaks = seq(1, max(gdat$x), 2)) +
+        ggplot2::scale_x_continuous(expand = c(0, 0), breaks = seq(1, max(gdat$y), 2)) +
         ggplot2::theme(legend.position = "none", aspect.ratio = 0.3) +
-        ggplot2::labs(x = paste(xnam, "Lag", sep = " "), y = paste(ynam, "Lag", sep = " "))
+        ggplot2::labs(y = paste(xnam, "Lag", sep = " "), x = paste(ynam, "Lag", sep = " "))
 
 
-        b <- lattice::wireframe(z ~ y * x, data = gdat, aspect = 0.5,
+        b <- lattice::wireframe(z ~ y * x, data = gdat, aspect = c(61/87, 0.4),
                                 scales = list(cex = 0.5, arrows = FALSE),
                                 shade = TRUE, colorkey = FALSE,
                                 par.settings = list(axis.line = list(col = 'transparent')),
                                 xlab = list(label = paste(ynam, "Lag", sep = " "), cex = .8, rot = 20),
                                 ylab = list(label = paste(xnam, "Lag", sep = " "), cex = .8, rot = -18),
                                 zlab = list(label = NULL, cex.axis = 0.5))
-            b <- latticeExtra::resizePanels(update(b, aspect  = 0.5))
 
-          b <- ggplotify::as.grob(b)
 
-        output <- patchwork::wrap_plots(b / a)
+        #b <- latticeExtra::resizePanels(update(b, aspect  = 0.75,
+        #                                           par.settings = list(fontsize = list(text = 8))))
+
+        #b <- ggplotify::as.grob(b)
+
+        output <- cowplot::plot_grid(b, a, nrow = 2, labels = c('A', 'B'), scale = c(2, 1))
+
+        output
 
         return(output)
 }
