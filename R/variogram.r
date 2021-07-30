05f310cbedc428ade8429f563ef65da2969c2600
Sharon Nielsen <65264926+snstats@users.noreply.github.com>
2021-07-30 17:30:52 +1000

    fix merge conflict and cowplot

--- R/variogram.r
+++ R/variogram.r
@@ -6,10 +6,24 @@
 #'
 #' @return A list containing ggplot2 objects.
 #'
+#' @importFrom akima interp interp2xyz
+#' @importFrom lattice wireframe
+#' @importFrom latticeExtra resizePanels
+#' @importFrom ggplotify as.grob
+#' @importFrom patchwork wrap_plots
+#' @importFrom ggplot2 ggplot geom_tile coord_equal geom_contour scale_fill_gradientn theme_bw scale_x_continuous scale_y_continuous theme labs
 #'
 #' @examples
-#' dat.asr <- asreml(Petal.Length ~ Petal.Width, data = iris)
-#' vario(dat.asr)
+#' \dontrun{
+#' library(asreml)
+#' oats <- asreml::oats
+#' oats <- oats[order(oats$Row, oats$Column),]
+#' model.asr <- asreml(yield ~ Nitrogen + Variety + Nitrogen:Variety,
+#'                     random = ~ Blocks + Blocks:Wplots,
+#'                     residual = ~ ar1(Row):ar1(Column),
+#'                     data = oats)
+#' vario(model.asr)
+#' }
 #' @export
 
 vario <- function(mod.obj){
@@ -24,7 +38,7 @@ vario <- function(mod.obj){
     fld <- akima::interp(y = aa[,1], x = aa[,2], z = aa$gamma)
     gdat <- akima::interp2xyz(fld, data.frame = TRUE)
 
-        a <- ggplot2::ggplot(gdat, ggplot2::aes(x = y, y = x, z = z, fill = z)) +
+    a <- ggplot2::ggplot(gdat, ggplot2::aes(x = y, y = x, z = z, fill = z)) +
         ggplot2::geom_tile(alpha = 0.6) +
         ggplot2::coord_equal() +
         ggplot2::geom_contour(color = "white", alpha = 0.5) +
@@ -36,26 +50,15 @@ vario <- function(mod.obj){
         ggplot2::labs(y = paste(xnam, "Lag", sep = " "), x = paste(ynam, "Lag", sep = " "))
 
 
-        b <- lattice::wireframe(z ~ y * x, data = gdat, aspect = c(61/87, 0.4),
-                                scales = list(cex = 0.5, arrows = FALSE),
-                                shade = TRUE, colorkey = FALSE,
-                                par.settings = list(axis.line = list(col = 'transparent')),
-                                xlab = list(label = paste(ynam, "Lag", sep = " "), cex = .8, rot = 20),
-                                ylab = list(label = paste(xnam, "Lag", sep = " "), cex = .8, rot = -18),
-                                zlab = list(label = NULL, cex.axis = 0.5))
+    b <- lattice::wireframe(z ~ y * x, data = gdat, aspect = c(61/87, 0.4),
+                            scales = list(cex = 0.5, arrows = FALSE),
+                            shade = TRUE, colorkey = FALSE,
+                            par.settings = list(axis.line = list(col = 'transparent')),
+                            xlab = list(label = paste(ynam, "Lag", sep = " "), cex = .8, rot = 20),
+                            ylab = list(label = paste(xnam, "Lag", sep = " "), cex = .8, rot = -18),
+                            zlab = list(label = NULL, cex.axis = 0.5))
 
+    output <- cowplot::plot_grid(b, a, nrow = 2, labels = c('A', 'B'), scale = c(2, 1))
 
-        #b <- latticeExtra::resizePanels(update(b, aspect  = 0.75,
-        #                                           par.settings = list(fontsize = list(text = 8))))
-
-        #b <- ggplotify::as.grob(b)
-
-        output <- cowplot::plot_grid(b, a, nrow = 2, labels = c('A', 'B'), scale = c(2, 1))
-
-        output
-
-        return(output)
+    return(output)
 }
-
-#' @rdname vario
-#' @export
