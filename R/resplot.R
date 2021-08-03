e79fbef7e15ca15ff5e7a410dc147d3ad63e6717
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-08-03 14:37:35 +0930

    Updated to use cowplot

--- R/resplot.R
+++ R/resplot.R
@@ -4,12 +4,13 @@
 #'
 #' @param mod.obj An `aov` or `asreml` model object.
 #' @param shapiro (Logical) Display the Shapiro-Wilks test of normality on the plot?
+#' @param label.size A numeric value for the size of the label font point size.
 #'
 #' @return A list containing ggplot2 objects which are diagnostic plots.
 #'
 #' @importFrom ggplot2 ggplot geom_histogram aes theme_bw stat_qq labs geom_qq_line geom_point
 #' @importFrom stats fitted qnorm quantile resid sd
-#' @importFrom patchwork wrap_plots plot_annotation
+#' @importFrom cowplot plot_grid add_sub
 #' @importFrom grid gpar
 #' @importFrom gridtext richtext_grob
 #' @importFrom stats shapiro.test
@@ -22,7 +23,7 @@
 #' resplt(dat.aov)
 #' @export
 
-resplot <- function(mod.obj, shapiro = TRUE){
+resplot <- function(mod.obj, shapiro = TRUE, label.size = 10){
 
     # Assign NULL to variables that give a NOTE in package checks
     # Known issue. See https://www.r-bloggers.com/no-visible-binding-for-global-variable/
@@ -63,15 +64,17 @@ resplot <- function(mod.obj, shapiro = TRUE){
 
         a <- ggplot2::ggplot(data = aa.f, mapping = ggplot2::aes(x = stdres)) +
             ggplot2::geom_histogram(bins = ifelse(nrow(aa) < 31, 7, 11), fill = "aquamarine3", colour = "black") +
-            ggplot2::theme_bw() + ggplot2::labs(y = "Frequency", x = "Standardised Residual")
+            ggplot2::theme_bw(base_size = 8) + ggplot2::labs(y = "Frequency", x = "Standardised Residual")
 
         b <- ggplot2::ggplot(aa.f, ggplot2::aes(sample = stdres)) + ggplot2::geom_qq(colour = "black", fill = "aquamarine3", size = 2 , shape = 21) +
-            ggplot2::geom_qq_line() + ggplot2::theme_bw() +
+            ggplot2::geom_qq_line() + ggplot2::theme_bw(base_size = 8) +
             ggplot2::labs(y = "Standardised Residual", x = "Theoretical")
 
         c <- ggplot2::ggplot(data = aa.f, mapping = ggplot2::aes(x = fitted, y = stdres)) +
-            ggplot2::geom_point(colour = "black", fill = "aquamarine3", size = 2 , shape = 21) + ggplot2::theme_bw() +
+            ggplot2::geom_point(colour = "black", fill = "aquamarine3", size = 2 , shape = 21) + ggplot2::theme_bw(base_size = 8) +
             ggplot2::labs(y = "Standardised Residual", x = "Fitted Value")
+
+        top_row <- cowplot::plot_grid(a, b, ncol=2, labels = c("A", "B"), label_size = label.size)
 
         if(shapiro) {
             shap <- shapiro.test(aa.f$residuals)
@@ -81,14 +84,19 @@ resplot <- function(mod.obj, shapiro = TRUE){
                                      paste0("The residuals appear to be normally distributed. (n = ", length(aa.f$residuals), ")"),
                                      paste0("The residuals do not appear to be normally distributed. (n = ", length(aa.f$residuals), ")")))
 
-            d <- gridtext::richtext_grob(shapiro_text, y = c(0.65, 0.4), gp = grid::gpar(fontsize = c(12, 10)))#,
-            output[[i]] <- patchwork::wrap_plots(A = a, B = b, C = c, D = d, design = "AB\nAB\nAB\nAB\nCC\nCC\nCC\nCC\nDD", tag_level = "new") +
-                patchwork::plot_annotation(tag_levels = list(c("A", "B", "C")),title = facet_name[i])
+            bottom_row <- cowplot::plot_grid(NULL, cowplot::add_sub(cowplot::add_sub(c, shapiro_text[1], size = 11, vjust = 1.2),
+                                                                    shapiro_text[2], size = 9, vjust = 0.2), NULL,
+                                             ncol=3, rel_widths=c(0.25,0.5,0.25), labels = c("", "C", ""),
+                                             label_size = label.size, hjust = 1)
+            # bottom_row
+            # d <- gridtext::richtext_grob(shapiro_text, y = c(0.65, 0.4), gp = grid::gpar(fontsize = c(12, 10)))#,
+            # output[[i]] <- patchwork::wrap_plots(A = a, B = b, C = c, D = d, design = "AB\nAB\nAB\nAB\nCC\nCC\nCC\nCC\nDD", tag_level = "new") +
+            # patchwork::plot_annotation(tag_levels = list(c("A", "B", "C")),title = facet_name[i])
         }
         else{
-            output[[i]] <- patchwork::wrap_plots(A = a, B = b, C = c, design = "AB\nCC", tag_level = "new") +
-                patchwork::plot_annotation(tag_levels = "A", title = facet_name[i])
+            bottom_row <- cowplot::plot_grid(NULL, c, NULL, ncol=3, rel_widths=c(0.25,0.5,0.25), labels = c("", "C", ""), hjust = 1, label_size = label.size)
         }
+        output[[i]] <- cowplot::plot_grid(top_row, bottom_row, ncol=1)
     }
 
     if(facet>1) {
@@ -98,13 +106,6 @@ resplot <- function(mod.obj, shapiro = TRUE){
     else {
         return(output[[1]])
     }
-
-
-    library(cowplot)
-    top_row <- cowplot::plot_grid(a, b, ncol=2, labels = c("A", "B"))
-    bottom_row <- cowplot::plot_grid(NULL, add_sub(c, "ABC"), NULL, ncol=3, rel_widths=c(0.25,0.5,0.25), labels = "C")
-
-    cowplot::plot_grid(top_row, bottom_row, ncol=1)
 }
 
 #' @rdname resplt
