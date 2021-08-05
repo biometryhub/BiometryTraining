deedb39a847ad25667a65fba85933cf2232f3dcc
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-08-05 20:21:05 +0930

    Updates to install_asreml

--- tests/testthat/teardown-install_asreml.R
+++ tests/testthat/teardown-install_asreml.R
@@ -2,13 +2,3 @@
 if(length(list.files(pattern = "asreml+(([a-zA-Z0-9_.\\-])*)+(.zip|.tar.gz|.tgz)", recursive = TRUE))>0) {
     file.remove(list.files(pattern = "asreml+(([a-zA-Z0-9_.\\-])*)+(.zip|.tar.gz|.tgz)", recursive = TRUE))
 }
-
-# if(length(list.files(path = "data", pattern = "asreml+(([a-zA-Z0-9_.\\-])*)+(.zip|.tar.gz|.tgz)"))>0) {
-#     for(i in 1:length(list.files(path = "data", pattern = "asreml+(([a-zA-Z0-9_.\\-])*)+(.zip|.tar.gz|.tgz)"))) {
-#         file.remove(list.files(path = "data", pattern = "asreml+(([a-zA-Z0-9_.\\-])*)+(.zip|.tar.gz|.tgz)", )[i])
-#     }
-# }
-
-# if(dir.exists("data")) {
-#     unlink("data", recursive = T)
-# }
