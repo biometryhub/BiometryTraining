084b74520c4e2b13b15709e5b34cafa2eb0df44f
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-11-23 22:58:46 +1030

    Sped up variogram function

--- R/variogram_stuff.R
+++ R/variogram_stuff.R
@@ -1,29 +1,32 @@
 # Comp to asreml
-library(asreml)
-oats <- asreml::oats
-oats <- oats[order(oats$Row, oats$Column),]
-model.asr <- asreml(yield ~ Nitrogen + Variety + Nitrogen:Variety,
-                    random = ~ Blocks + Blocks:Wplots,
-                    residual = ~ ar1(Row):ar1(Column),
-                    data = oats)
+# library(asreml)
+# oats <- asreml::oats
+# oats <- oats[order(oats$Row, oats$Column),]
+# model.asr <- asreml(yield ~ Nitrogen + Variety + Nitrogen:Variety,
+#                     random = ~ Blocks + Blocks:Wplots,
+#                     residual = ~ ar1(Row):ar1(Column),
+#                     data = oats)
 
 
 vario <- function(model.obj) {
     # So the 'z' value for the variogram is the residuals
     # Need to be able to pull out the x/y from the model object
 
-    vals <- model.obj$mf[, c('Row', 'Column')]
-    vals <- cbind(vals, resid = resid(model.obj))
+    dims <- unlist(strsplit(names(model.obj$R.param[1]), ":"))
+    vals <- cbind(data.frame(model.obj$mf[[dims[1]]]), data.frame(model.obj$mf[[dims[2]]]), resid = resid(model.obj))
+    colnames(vals) <- c(dims, "resid")
+    Row <- as.numeric(model.obj$mf[[dims[1]]])
+    Column <- as.numeric(model.obj$mf[[dims[2]]])
+    Resid <- residuals(model.obj)
 
+    nrows <- max(Row)
+    ncols <- max(Column)
 
-    rows <- max(as.numeric(vals$Row))
-    cols <- max(as.numeric(vals$Column))
-
-    vario <- expand.grid(Row = 0:(rows-1), Column = 0:(cols-1))
+    vario <- expand.grid(Row = 0:(nrows-1), Column = 0:(ncols-1))
 
     # Ignore the 0, 0 case (gamma=0, counted row*cols times)
-    gammas <- c(0)
-    nps <- c(rows * cols)
+    gammas <- rep(0, nrows*ncols)
+    nps <- rep(nrows*ncols, nrows*ncols)
 
     for (index in 2:nrow(vario)) {
         i <- vario[index, 'Row']
@@ -32,18 +35,18 @@ vario <- function(model.obj) {
         gamma <- 0
         np <- 0
         for (val_index in 1:nrow(vals)) {
-            val <- vals[val_index, ]
+            # val <- vals[val_index, ]
 
             # Deliberate double-counting so that offset handling is easy
             # (so e.g. we compute distance from (1,1)->(2,3), and then again
             # later from (2,3)->(1,1)).
             for (offset in unique(list(c(i, j), c(-i, j), c(i, -j), c(-i, -j)))) {
-                row <- as.numeric(val['Row']) + offset[1]
-                col <- as.numeric(val['Column']) + offset[2]
+                row <- Row[val_index] + offset[1]
+                col <- Column[val_index] + offset[2]
 
-                if (0 < row && row <= rows && 0 < col && col <= cols) {
-                    other <- vals[which(vals['Row'] == row & vals['Column'] == col), ]
-                    gamma <- gamma + (as.numeric(val['resid'])-as.numeric(other['resid']))^2
+                if (0 < row && row <= nrows && 0 < col && col <= ncols) {
+                    other <- which(Row == row & Column == col)
+                    gamma <- gamma + (Resid[val_index]-Resid[other])^2
                     np <- np + 1
                 }
             }
@@ -56,13 +59,36 @@ vario <- function(model.obj) {
             gamma <- gamma / (2*np)
         }
 
-        gammas <- append(gammas, gamma)
-        nps <- append(nps, np)
+        gammas[index] <- gamma
+        nps[index] <- np
     }
     vario <- cbind(vario, data.frame(gamma = gammas, np = nps))
+    colnames(vario) <- c(dims, "gamma", "np")
+    class(vario) <- c("varioGram", "data.frame")
+    return(vario)
 }
 
 # Direct comparison
 # View(cbind(vario, asreml::varioGram(model.asr)))
 
-# vario(model.asr)
+# v1 <- vario(model.asr)
+# v2 <- asreml::varioGram(model.asr)
+# cbind(v1, v2)
+#
+# library(dplyr)
+# new_data <- design(type = "split", 1:5, sub_treatments = 1:20, 4, 20, 20, brows = 5, bcols = 20, seed = 42, quiet = T)
+# new_data <- new_data$design
+#
+# new_data <- new_data[order(new_data$row, new_data$col),]
+# new_data$yield <- rnorm(400)
+#
+# new_data <- new_data %>% mutate(across(1:7, factor))
+#
+# model.asr <- asreml(yield ~ treatments * sub_treatments,
+#                     random = ~ block + block:plots,
+#                     residual = ~ ar1(row):ar1(col),
+#                     data = new_data)
+#
+# v1 <- vario(model.asr)
+# v2 <- asreml::varioGram(model.asr)
+
