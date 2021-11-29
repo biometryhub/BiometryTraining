70142191af044c14a153e9563fbd74218bafe96d
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-11-29 16:24:03 +1030

    Fixing up test for sommer without needing ASreml

--- tests/testthat/test-resplot.R
+++ tests/testthat/test-resplot.R
@@ -9,14 +9,14 @@ test_that("Residual plots work for aov", {
 })
 
 test_that("resplot produces an error for invalid data types work for aov", {
-    expect_error(resplot(1:10), "model.obj must be an aov, lm, lmerMod, lmerModLmerTest, asreml or mmer object")
+  expect_error(resplot(1:10), "model.obj must be an aov, lm, lmerMod, lmerModLmerTest, asreml or mmer object")
 })
 
 test_that("Old mod.obj argument produces a warning", {
-    dat.aov <- aov(Petal.Length ~ Petal.Width, data = iris)
-    expect_warning(p <- resplot(mod.obj = dat.aov),
-                   "mod.obj has been deprecated to be consistent with other functions. Please use `model.obj` instead.")
-    vdiffr::expect_doppelganger(title = "Resplot after warning", p)
+  dat.aov <- aov(Petal.Length ~ Petal.Width, data = iris)
+  expect_warning(p <- resplot(mod.obj = dat.aov),
+                 "mod.obj has been deprecated to be consistent with other functions. Please use `model.obj` instead.")
+  vdiffr::expect_doppelganger(title = "Resplot after warning", p)
 })
 
 test_that("Residual plots work for asreml", {
@@ -55,10 +55,10 @@ test_that("Residual plots work for lme4", {
 test_that("Residual plots work for nlme", {
   skip_if_not_installed("nlme")
   dat.nlme <- nlme::nlme(height ~ SSasymp(age, Asym, R0, lrc),
-                    data = Loblolly,
-                    fixed = Asym + R0 + lrc ~ 1,
-                    random = Asym ~ 1,
-                    start = c(Asym = 103, R0 = -8.5, lrc = -3.3))
+                         data = Loblolly,
+                         fixed = Asym + R0 + lrc ~ 1,
+                         random = Asym ~ 1,
+                         start = c(Asym = 103, R0 = -8.5, lrc = -3.3))
 
   p1 <- resplot(dat.nlme)
 
@@ -68,10 +68,11 @@ test_that("Residual plots work for nlme", {
 
 test_that("Residual plots work for sommer", {
   skip_if_not_installed("sommer")
+  load("../asreml_oats.Rdata")
   dat.som <- sommer::mmer(yield ~ Nitrogen + Variety + Nitrogen:Variety,
-                    random = ~ Blocks + Blocks:Wplots,
-                    rcov = ~ units,
-                    data = asreml::oats)
+                          random = ~ Blocks + Blocks:Wplots,
+                          rcov = ~ units,
+                          data = dat)
 
   p1 <- resplot(dat.som)
   # p2 <- resplot(dat.aov, shapiro = FALSE)
@@ -81,12 +82,12 @@ test_that("Residual plots work for sommer", {
 })
 
 test_that("Residual plots display call for aov and lm", {
-    # dat.aov <-
-    dat.aov <- aov(Petal.Length ~ Petal.Width, data = iris)
-    p1 <- resplot(dat.aov, call = TRUE)
-    p2 <- resplot(dat.aov, call = TRUE, call.size = 7)
+  # dat.aov <-
+  dat.aov <- aov(Petal.Length ~ Petal.Width, data = iris)
+  p1 <- resplot(dat.aov, call = TRUE)
+  p2 <- resplot(dat.aov, call = TRUE, call.size = 7)
 
-    vdiffr::expect_doppelganger(title = "Resplot with call", p1)
-    vdiffr::expect_doppelganger(title = "Resplot with smaller call", p2)
-    # vdiffr::expect_doppelganger(title = "Resplot for aov without shapiro", p2)
+  vdiffr::expect_doppelganger(title = "Resplot with call", p1)
+  vdiffr::expect_doppelganger(title = "Resplot with smaller call", p2)
+  # vdiffr::expect_doppelganger(title = "Resplot for aov without shapiro", p2)
 })
